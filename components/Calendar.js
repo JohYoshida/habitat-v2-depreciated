4569271a301dce0c60b5a72e85eedf7621dc75bb
Joh Yoshida <johyoshida@gmail.com>
2018-11-12 16:32:36 -0800

    Refactor calendar

--- components/Calendar.js
+++ components/Calendar.js
@@ -14,49 +14,111 @@ class Calendar extends Component {
       Jul: 31, Aug: 31, Sep: 30, Oct: 31, Nov: 30, Dec: 31,
     };
 
-    const year = [];
-    for (var month in months) {
-      const days = [];
-      for (var i = 1; i <= months[month]; i++) {
-        let key = `${month}-${i}`
-        days.push(
-          <TouchableOpacity key={key} onPress={this._onPress.bind(this)}>
-            <View style={styles.days} >
-              <Text>{ i }</Text>
+    const Months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    const Year = [];
+
+    Months.forEach(month => {
+      const { data } = this.props;
+      for (var obj in data) {
+        if (month === obj) {
+          // console.log(month, data[month]);
+          const Days = [];
+          for (var i = 1; i <= data[month].length; i++) {
+            let key = `${month}-${i}`;
+            console.log(month, i, typeof(data[month][i - 1]));
+            if (data[month][i - 1] === "1") {
+              Days.push(
+                <TouchableOpacity
+                  key={key}
+                  onLongPress={this._onLongPress.bind(this, this.props.habit, key)}
+                  >
+                  <View style={styles.complete} >
+                    <Text>{ i }</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            } else {
+              Days.push(
+                <TouchableOpacity
+                  key={key}
+                  onLongPress={this._onLongPress.bind(this, this.props.habit, key)}
+                >
+                  <View style={styles.incomplete} >
+                    <Text>{ i }</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            }
+          }
+          Year.push(
+            <View style={styles.month} key={month}>
+              <Text>{ month }</Text>
+              { Days }
             </View>
-          </TouchableOpacity>
-        );
+          );
+
+        }
       }
-      year.push(
-        <View style={styles.month} key={month}>
-          <Text>{ month }</Text>
-          { days }
-        </View>
-      );
-    }
+    })
+
+    // for (var month in months) {
+    //   const days = [];
+    //   for (var i = 1; i <= months[month]; i++) {
+    //     let key = `${month}-${i}`;
+    //     days.push(
+    //       <TouchableOpacity
+    //         key={key}
+    //         onLongPress={this._onLongPress.bind(this, this.props.habit, key)}
+    //       >
+    //         <View style={styles.complete} >
+    //           <Text>{ i }</Text>
+    //         </View>
+    //       </TouchableOpacity>
+    //     );
+    //   }
+    //   Year.push(
+    //     <View style={styles.month} key={month}>
+    //       <Text>{ month }</Text>
+    //       { days }
+    //     </View>
+    //   );
+    // }
 
     return (
       <ScrollView>
         <Text style={styles.text}>{this.props.habit}</Text>
         <View style={styles.year}>
-          { year }
+          { Year }
         </View>
       </ScrollView>
     );
   }
 
-  _onPress() {
-
+  _onLongPress(habit, key) {
+    if (this.props.days[key]) {
+      this.props.toggle(habit, key, false);
+    } else {
+      this.props.toggle(habit, key, true);
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  days: {
+  complete: {
     flex: 0,
     borderWidth: 1,
     height: 40,
     margin: 1,
-    backgroundColor: "#486cc9",
+    backgroundColor: "#3672FD",
+  },
+  incomplete: {
+    flex: 0,
+    borderWidth: 1,
+    height: 40,
+    margin: 1,
+    backgroundColor: "#adc4f9",
   },
   month: {
     flex: 1,
@@ -70,7 +132,7 @@ const styles = StyleSheet.create({
   year: {
     flex: 1,
     flexDirection: "row",
-    marginTop: 24,
+    marginTop: 12,
   },
 });
 
