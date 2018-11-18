7f9c04acc3b66890c8fc1f745f0f01a53fdb39ad
Joh Yoshida <johyoshida@gmail.com>
2018-11-18 01:02:50 -0800

    Animated loading panel

--- components/LoadingPanel.js
+++ components/LoadingPanel.js
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Animated, Easing, StyleSheet, Text, View } from "react-native";
 
 class LoadingPanel extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loadingAnim: new Animated.Value(0),
+      colorAnim: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    Animated.loop(
+      Animated.parallel([
+        Animated.timing(this.state.loadingAnim, {
+          toValue: 1,
+          duration: 2000,
+        }),
+        Animated.timing(this.state.colorAnim, {
+          toValue: 1,
+          duration: 2000,
+        }),
+      ])
+    ).start();
   }
 
   render() {
+    const { loadingAnim, colorAnim } = this.state;
     return (
-      <View style={styles.container}>
-        <View style={styles.filler} />
-        <View style={styles.filler} />
-        <View style={styles.ball} />
-      </View>
+      <Animated.View
+        style={{
+          transform: [{
+            rotate: loadingAnim.interpolate({
+              inputRange: [0, 1],
+              outputRange: ["0deg", "360deg"]
+            }),
+          }],
+        }}>
+        <View style={styles.container}>
+          <View style={styles.spacer} />
+          <Animated.View style={{
+              backgroundColor: colorAnim.interpolate({
+                inputRange: [0, 0.33, 0.66,  1],
+                outputRange: ["#f44336", "#FFEB3B", "#2196F3", "#f44336"]
+              }),
+              borderRadius: 100
+            }}>
+            <View style={styles.ball} />
+          </Animated.View>
+        </View>
+      </Animated.View>
     );
   }
 }
@@ -22,19 +58,15 @@ const styles = StyleSheet.create({
   container: {
     alignSelf: "center",
     alignItems: "center",
-    backgroundColor: "#0D47A1",
     borderRadius: 100,
+    height: 150,
   },
-  filler: {
-    backgroundColor: "#54abf0",
-    height: 50,
-    width: 50,
-    borderRadius: 100,
+  spacer: {
+    height: 100,
   },
   ball: {
-    backgroundColor: "#2196F3",
-    height: 50,
-    width: 50,
+    height: 25,
+    width: 25,
     borderRadius: 100,
   },
 });
