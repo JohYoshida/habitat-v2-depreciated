2eec7b0dd548486fdfbdf0172737aaccd0e8cd71
Joh Yoshida <johyoshida@gmail.com>
2018-11-19 02:36:51 -0800

    Skeleton

---
+++ components/LoginModal.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Alert, Button, Modal, Text, TextInput, StyleSheet, View } from "react-native";
+
+class LoginModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+    );
+
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default LoginModal;
