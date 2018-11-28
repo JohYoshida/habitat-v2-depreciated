26be9afa21dc8d52e3be2118aeec21034d795c98
Joh Yoshida <johyoshida@gmail.com>
2018-11-27 22:40:14 -0800

    Authentication flow

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createSwitchNavigator } from "react-navigation";
 
-import MainTabNavigator from './MainTabNavigator';
+import AuthNavigator from "./AuthNavigator";
+import MainTabNavigator from "./MainTabNavigator";
+import AuthLoadingScreen from "../screens/AuthLoadingScreen";
 
-export default createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-});
+// You could add another route here for authentication.
+// Read more at https://reactnavigation.org/docs/en/auth-flow.html
+export default createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    Auth: AuthNavigator,
+    App: MainTabNavigator
+  },
+  {
+    initialRouteName: "AuthLoading"
+  }
+);
