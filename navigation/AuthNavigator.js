9d7e316545b10ede1b8656b139e777a60af11d33
Joh Yoshida <johyoshida@gmail.com>
2018-11-28 00:16:01 -0800

    Add RegisterScreen

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Platform } from "react-native";
-import { createStackNavigator } from "react-navigation";
+import { createSwitchNavigator } from "react-navigation";
 
 import SignInScreen from "../screens/SignInScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
-export default createStackNavigator({
-  SignIn: SignInScreen
-});
+export default createSwitchNavigator(
+  {
+  SignIn: SignInScreen,
+  Register: RegisterScreen,
+},
+{
+  initialRouteName: "SignIn"
+}
+);
