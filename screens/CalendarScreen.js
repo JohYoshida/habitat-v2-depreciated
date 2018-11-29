000887e6342a65315eadad7a8386bd7693b20e01
Joh Yoshida <johyoshida@gmail.com>
2018-11-28 22:45:29 -0800

    Skeleton

---
+++ screens/CalendarScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+export default class CalendarScreen extends React.Component {
+  static navigationOptions = {
+    // header: null,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
