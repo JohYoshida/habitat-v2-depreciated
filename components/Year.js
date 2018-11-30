aedbe705e1dfc5b6cb8b7cca33e7d3117a0c3479
Joh Yoshida <johyoshida@gmail.com>
2018-11-29 21:03:25 -0800

    Get and store relevant calendar data in state

--- components/Year.js
+++ components/Year.js
@@ -7,12 +7,34 @@ const { URL } = require("../constants");
 class Year extends React.Component {
   static navigationOptions = {
     // header: null,
-  }
+  };
 
   constructor(props) {
     super(props);
     this.state = {
+      userToken: "",
+      authString: "",
+      year: moment().year(),
+      data: {}
     };
+  }
+
+  componentDidMount() {
+    const { habitID } = this.props;
+    this._getAsyncKeys().then(res => {
+      const { userToken, authString } = res;
+      let { year, data } = this.state;
+      // Get days associated with habit calendar
+      fetch(`${URL}/users/${userToken}/habits/${habitID}/${year}`)
+        .then(res => res.json())
+        .then(json => {
+          json.forEach(row => {
+            data[`${row.month}-${row.day}`] = row.value;
+          });
+          this.setState({ data, userToken, authString });
+        })
+        .catch(err => console.log("Error!", err));
+    });
   }
 
   render() {
