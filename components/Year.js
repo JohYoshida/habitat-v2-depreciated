61a0300ab866d014009ed4b8f52ed7fade832af1
Joh Yoshida <johyoshida@gmail.com>
2018-11-29 20:09:54 -0800

    Skeleton

---
+++ components/Year.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { AsyncStorage, ScrollView, StyleSheet, Text, TouchableOpacity,  View } from "react-native";
+
+const moment = require("moment");
+const { URL } = require("../constants");
+
+class Year extends React.Component {
+  static navigationOptions = {
+    // header: null,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+    );
+  }
+const styles = StyleSheet.create({
+});
+
+export default Year;
