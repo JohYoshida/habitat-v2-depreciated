44a60c04b7225b02da06c41e97ebf8c88f02b598
Joh Yoshida <johyoshida@gmail.com>
2018-11-29 20:59:42 -0800

    Export array of Months

--- constants.js
+++ constants.js
@@ -1,5 +1,19 @@
 const URL = "https://habitat-exp.herokuapp.com";
+const Months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
 
 module.exports = {
-  URL
+  URL, Months
 }
